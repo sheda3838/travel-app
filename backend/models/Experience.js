3b028f44fb60fecd02de712f8321baf8d931f501
Zaid Kamil <kamilzaid53@gmail.com>
2026-03-10 05:57:02 +0530

    feat(models): add User and Experience schemas

---
+++ backend/models/Experience.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const experienceSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }, //creates createdAt & updatedAt automatically
+);
+
+module.exports = mongoose.model("Experience", experienceSchema);
