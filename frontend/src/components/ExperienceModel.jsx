2021dba1d4f69ae6421fcefe607fb0f67db63bbf
Zaid Kamil <kamilzaid53@gmail.com>
2026-03-11 07:32:48 +0530

    style(ui): add icons for edit/delete in card and location ping in model

--- frontend/src/components/ExperienceModel.jsx
+++ frontend/src/components/ExperienceModel.jsx
@@ -55,6 +55,10 @@ function ExperienceModel({ experience, onDelete }) {
         </div>
 
         <div className="flex items-center text-gray-600 mb-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-2 text-primary">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+          </svg>
           <span className="text-lg">{experience.location}</span>
         </div>
 
